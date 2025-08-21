88c19f9776ec3ea56a06a53619dd25c3a9897ff9
Omar-Irfan <omar.irfan18@gmail.com>
2025-08-20 22:01:42 -0400

    made some more front end edits, added banner, ensured buttons worked

--- components/announcement-banner.vue
+++ components/announcement-banner.vue
@@ -1,27 +1,31 @@
 <template>
   <div
     class="w-full z-50 bg-gradient-to-r from-orange-600 via-red-500 to-pink-500 
-           text-white text-center text-base sm:text-lg font-extrabold 
+           text-white text-center text-base sm:text-lg font-black 
            py-4 px-6 shadow-md"
   >
     <div class="inline-flex items-center flex-nowrap">
       <!-- Mobile: Clickable short text -->
-      <a 
-        href="#contact-section" 
+      <button 
+        @click="scrollToContact"
         class="inline whitespace-nowrap sm:hidden transition-colors duration-300 hover:text-yellow-200"
       >
-        Concept to Prototype starting at $5K!
-      </a>
+        Prototype your ideas! Starting at $5K!
+    </button>
       <!-- Desktop: Full text and separate link -->
       <span class="hidden whitespace-nowrap sm:inline">
-        From Concept to Prototype: Starting at $5K, delivered in just 2 months!
+        Turn concepts into prototypes in as little as 2 months! Starting from just $5K.
       </span>
-      <a 
-        href="#contact-section" 
+      <button 
+        @click="scrollToContact" 
         class="hidden whitespace-nowrap underline ml-2 sm:inline transition-colors duration-300 hover:text-yellow-200"
       >
         Find Out More!
-      </a>
+    </button>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { scrollToContact } from '~/lib/utils';
+</script>
