6ecfbc2a5c41350eb3b5db0eebda77c485f0718e
Harun Sheikhali <sufisheikhali@gmail.com>
2025-08-30 20:09:44 -0400

    push fixed changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Navigation />
-    <AccouncementBanner />
+    <AnnouncementBanner />
     <Hero />
     <ProcessSection />
     <AboutSection />
@@ -13,13 +13,13 @@
 </template>
 
 <script setup lang="ts">
-import Navigation from '@/components/navigation.vue';
-import AccouncementBanner from '~/components/accouncement-banner.vue';
-import Hero from '@/components/hero.vue';
-import ProcessSection from '@/components/process-section.vue';
-import AboutSection from '@/components/about-section.vue';
-import TeamSection from '@/components/team-section.vue';
-import TestimonialsSection from '@/components/testimonials-section.vue';
-import ContactSection from '@/components/contact-section.vue';
-import Footer from '@/components/footer.vue';
+import Navigation from "@/components/navigation.vue";
+import AnnouncementBanner from "~/components/announcement-banner.vue";
+import Hero from "@/components/hero.vue";
+import ProcessSection from "@/components/process-section.vue";
+import AboutSection from "@/components/about-section.vue";
+import TeamSection from "@/components/team-section.vue";
+import TestimonialsSection from "@/components/testimonials-section.vue";
+import ContactSection from "@/components/contact-section.vue";
+import Footer from "@/components/footer.vue";
 </script>
